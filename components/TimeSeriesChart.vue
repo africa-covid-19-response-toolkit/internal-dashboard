90ba8423333ec20ff7ec064f67e26fb8264c3af6
Yohannes Libanos <4yohannes@gmail.com>
2020-04-03 00:03:05 +0100

    Add TimeSeriesChart

---
+++ components/TimeSeriesChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card tile>
+    <v-card-title>{{ title }}</v-card-title>
+    <apexchart
+      type="line"
+      width="100%"
+      height="350"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["title", "chartdata"],
+  data() {
+    const { series, ...chartOptions } = this.$props.chartdata;
+
+    return {
+      series,
+      chartOptions: {
+        ...chartOptions
+      }
+    };
+  },
+  mounted() {}
+};
+</script>
