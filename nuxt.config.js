46f2811f1e9842b8e2bc7f87ba3f7dbd8d948a4d
Yohannes Ejigu <yohane55f@gmail.com>
2020-04-23 22:09:23 +0300

    add gzip compression

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,8 +73,45 @@ export default {
     ["@nuxtjs/pwa", { Icon: false }],
     "@nuxtjs/toast",
     "nuxt-leaflet",
-    "@nuxtjs/axios"
+    "@nuxtjs/axios",
+    "nuxt-precompress"
   ],
+
+  nuxtPrecompress: {
+    enabled: true, // Enable in production
+    report: true, // set false to turn off console messages
+    test: /\.(js|css|html|txt|xml|svg)$/, // files to compress on build
+    // Serving options
+    middleware: {
+      // You can disable middleware if you serve static files using nginx...
+      enabled: true,
+      // Enable if you have .gz or .br files in /static/ folder
+      enabledStatic: true,
+      // Priority of content-encodings, first matched with request Accept-Encoding will me served
+      encodingsPriority: ["br", "gzip"]
+    },
+    // build time compression settings
+    gzip: {
+      // should compress to gzip?
+      enabled: true,
+      // compression config
+      // https://www.npmjs.com/package/compression-webpack-plugin
+      filename: "[path].gz[query]", // middleware will look for this filename
+      threshold: 10240,
+      minRatio: 0.8,
+      compressionOptions: { level: 9 }
+    },
+    brotli: {
+      // should compress to brotli?
+      enabled: true,
+      // compression config
+      // https://www.npmjs.com/package/compression-webpack-plugin
+      filename: "[path].br[query]", // middleware will look for this filename
+      compressionOptions: { level: 11 },
+      threshold: 10240,
+      minRatio: 0.8
+    }
+  },
 
   toast: {
     position: "top-right",
